6a56c66ed44aee9dda65b9c1d69b20affdc18fea
Kamila <kamilaalvesdemiranda@gmail.com>
2023-12-04 22:37:50 -0300

    Reenviando código atualizado

--- funcionalidades/3_MostrarSaldo.c
+++ funcionalidades/3_MostrarSaldo.c
@@ -1,56 +1,62 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
     char titular[50];
-    char cpf[15];
     int numeroConta;
-    float saldo;
-} criarConta;
+    double saldo;
+} ContaBancaria;
 
-void mostrarSaldo(criarConta contas[], int numContas) {
-    // Variável para armazenar o número da conta inserido pelo usuário
+void mostrarSaldo(ContaBancaria contas[], int numContas) {
     int numeroConta;
+    char nomeTitular[50];
 
-    // Solicita ao usuário que insira o número da conta
     printf("Digite o numero da conta: ");
-    
-    // Adiciona verificação de entrada
-    if (scanf("%4d", &numeroConta) != 1 || numeroConta < 1000 || numeroConta > 9999) {
-        // Se a entrada não for um número de quatro dígitos, exibe mensagem de erro e encerra a função
-        printf("Numero de conta invalido.\n");
-        return;
-    }
+    scanf("%d", &numeroConta);
 
-    // Variável para armazenar o índice da conta no array, inicializado como -1 (não encontrado)
+    printf("Digite o nome do titular: ");
+    scanf("%s", nomeTitular);
+
     int indiceConta = -1;
-
-    // Loop para percorrer o array de contas
     for (int i = 0; i < numContas; i++) {
-        // Verifica se o número da conta na posição atual é igual ao número inserido pelo usuário
-        if (contas[i].numeroConta == numeroConta) {
-            // Se encontrado, armazena o índice e sai do loop
+        if (contas[i].numeroConta == numeroConta && strcmp(contas[i].titular, nomeTitular) == 0) {
             indiceConta = i;
             break;
         }
     }
 
-    // Verifica se a conta foi encontrada
-    if (indiceConta != -1 && indiceConta < numContas) {
-        // Imprime as informações da conta, incluindo titular, CPF, número da conta e saldo
-        printf("Saldo da conta de %s (CPF: %s), numero da conta: %d: %.2f\n",
-               contas[indiceConta].titular, contas[indiceConta].cpf,
-               contas[indiceConta].numeroConta, contas[indiceConta].saldo);
+    if (indiceConta != -1) {
+        printf("Saldo da conta de %s: %.2f\n", contas[indiceConta].titular, contas[indiceConta].saldo);
     } else {
-        printf("Conta com numero %d nao encontrada ou indice invalido.\n", numeroConta);
+        printf("Conta nao encontrada.\n");
     }
 }
 
 int main() {
-    criarConta contas[3] = {{"Titular1", "123.456.789-01", 1001, 1000.0},
-                            {"Titular2", "987.654.321-01", 1002, 1500.0},
-                            {"Titular3", "111.222.333-44", 1003, 2000.0}};
+    ContaBancaria contas[100];
+    int numContas = 0;
+    int opcao;
 
-    mostrarSaldo(contas, 3);
+    do {
+        printf("\nEscolha uma opcao:\n");
+        printf("3. Mostrar saldo total de cada conta\n");
+        printf("0. Sair\n");
+
+        printf("Digite o numero da opcao desejada: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 3:
+                mostrarSaldo(contas, numContas);
+                break;
+            case 0:
+                printf("Saindo do programa. Obrigado!\n");
+                break;
+            default:
+                printf("Opção invalida. Tente novamente!!!\n");
+        }
+
+    } while (opcao != 0);
 
     return 0;
 }
