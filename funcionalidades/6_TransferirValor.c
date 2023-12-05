e6d0a482608ca8764070a84472d15520cbeafbfe
Cassio <cassiojr0108@gmail.com>
2023-12-05 19:44:55 -0300

    refatorando funcionalidade 6

--- funcionalidades/6_TransferirValor.c
+++ funcionalidades/6_TransferirValor.c
@@ -1,88 +1,122 @@
-//cabeçalhos do compilador
+// transferencia.c
+
 #include <stdio.h>
 #include <string.h>
-//cabeçalhos do projeto
 #include "../headers/conta.h"
 
-void TransferirValor(){
-// Função para transferir valor de uma conta para outra
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    //VARIÁVEIS
-    char numContaOrigem[9];
-    char numContaDestino[9];
-    char dirContaOrigem[255];
-    char dirContaDestino[255];
-    double transferencia;
-    conta contaOrigem;
-    conta contaDestino;
+// Estrutura para representar uma conta bancária
+typedef struct {
+    char numero[20];
+    char titular[100];
+    double saldo;
+} ContaBancaria;
 
-    // Solicitar informações da transferência ao usuário
-    printf("\nDigite o numero da conta de origem: ");
-    scanf("%s", numContaOrigem);
-
-    printf("Digite o numero da conta de destino: ");
-    scanf("%s", numContaDestino);
-
-    printf("Digite o valor a ser transferido: R$ ");
-    scanf("%lf", &transferencia);
-
-
-    //diretório da conta origem
-    strcpy(dirContaOrigem,"./contas/");
-    strcat(dirContaOrigem,numContaOrigem);
-    strcat(dirContaOrigem,".csv");
-    //diretório da conta destino
-    strcpy(dirContaDestino,"./contas/");
-    strcat(dirContaDestino,numContaDestino);
-    strcat(dirContaDestino,".csv");
-
-    //verifica se a conta origem existe
-    if(contaExiste(dirContaOrigem)){
-        printf("A conta %s nao existe!\n",numContaOrigem);
-        return;
-    }else{
-        contaOrigem = pesquisarConta(numContaOrigem);
-    }
-    //verifica se a conta destino existe
-    if(contaExiste(dirContaDestino)){
-        printf("A conta %s nao existe!\n",numContaDestino);
-    }else{
-        contaDestino = pesquisarConta(numContaDestino);
+void salvarContaNoArquivo(const char *nomeArquivo, const ContaBancaria *conta) {
+    FILE *arquivo = fopen(nomeArquivo, "w");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo para escrita");
+        exit(EXIT_FAILURE);
     }
 
-    //ANTES DA OPERAÇÃO
-    printf("\nDADOS ANTES DA OPERACAO\n");
-    printf("\n\tConta origem\n");
-    printf("\tNumero: %s\n",contaOrigem.numero);
-    printf("\tTitular: %s\n",contaOrigem.titular);
-    printf("\tSaldo: R$ %.2lf\n",contaOrigem.saldo);
-    printf("\n\tConta destino\n");
-    printf("\tNumero: %s\n",contaDestino.numero);
-    printf("\tTitular: %s\n",contaDestino.titular);
-    printf("\tSaldo: %.2lf\n",contaDestino.saldo);
+    fprintf(arquivo, "%s,%s,%.2lf", conta->numero, conta->titular, conta->saldo);
 
+    fclose(arquivo);
+}
 
-    //verifica a conta origem possui saldo suficiente
-    if(contaOrigem.saldo < transferencia){
-        printf("A conta %s nao possui saldo suficiente para transferencia!\n",contaOrigem.numero);
-        return;
-    }else{
-        //realiza a transferência
-        contaOrigem.saldo -= transferencia;
-        contaDestino.saldo += transferencia;
-        salvarConta(contaOrigem);
-        salvarConta(contaDestino);
+void lerContaDoArquivo(const char *nomeArquivo, ContaBancaria *conta) {
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo para leitura");
+        exit(EXIT_FAILURE);
     }
 
-    //DEPOIS DA OPERAÇÃO
+    fscanf(arquivo, "%19[^,],%99[^,],%lf", conta->numero, conta->titular, &conta->saldo);
+
+    fclose(arquivo);
+}
+
+void transferirSaldo(const char *numContaOrigem, const char *numContaDestino, double valor) {
+    char dirContaOrigem[100];
+    char dirContaDestino[100];
+
+    // Construir o caminho do arquivo para a conta de origem
+    strcpy(dirContaOrigem, "../contas/");
+    strcat(dirContaOrigem, numContaOrigem);
+    strcat(dirContaOrigem, ".csv");
+
+    // Construir o caminho do arquivo para a conta de destino
+    strcpy(dirContaDestino, "../contas/");
+    strcat(dirContaDestino, numContaDestino);
+    strcat(dirContaDestino, ".csv");
+
+    // Abrir arquivo da conta de origem para leitura
+    ContaBancaria contaOrigem;
+    lerContaDoArquivo(dirContaOrigem, &contaOrigem);
+
+    // Validar se há saldo suficiente na conta de origem
+    if (contaOrigem.saldo < valor) {
+        printf("Saldo insuficiente para realizar a transferência.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Abrir arquivo da conta de destino para leitura
+    ContaBancaria contaDestino;
+    lerContaDoArquivo(dirContaDestino, &contaDestino);
+
+    // Realizar a transferência
+    contaOrigem.saldo -= valor;
+    contaDestino.saldo += valor;
+
+    // Salvar contas atualizadas nos arquivos
+    salvarContaNoArquivo(dirContaOrigem, &contaOrigem);
+    salvarContaNoArquivo(dirContaDestino, &contaDestino);
+
+    // Exibir dados após a operação
     printf("\nDADOS DEPOIS DA OPERACAO\n");
     printf("\n\tConta origem\n");
-    printf("\tNumero: %s\n",contaOrigem.numero);
-    printf("\tTitular: %s\n",contaOrigem.titular);
-    printf("\tSaldo: R$ %.2lf\n",contaOrigem.saldo);
+    printf("\tNumero: %s\n", contaOrigem.numero);
+    printf("\tTitular: %s\n", contaOrigem.titular);
+    printf("\tSaldo: R$ %.2lf\n", contaOrigem.saldo);
     printf("\n\tConta destino\n");
-    printf("\tNumero: %s\n",contaDestino.numero);
-    printf("\tTitular: %s\n",contaDestino.titular);
-    printf("\tSaldo: %.2lf\n",contaDestino.saldo);
+    printf("\tNumero: %s\n", contaDestino.numero);
+    printf("\tTitular: %s\n", contaDestino.titular);
+    printf("\tSaldo: R$ %.2lf\n", contaDestino.saldo);
+}
 
+
+
+void TransferirValor() {
+    // char numContaOrigem[9];
+    // char numContaDestino[9];
+    // char teste[9];
+    // char dirContaOrigem[255];
+    // char dirContaDestino[255];
+    // double transferencia;
+    // ContaBancaria contaOrigem;
+    // ContaBancaria contaDestino;
+
+
+    char numContaOrigem[20];
+    char numContaDestino[20];
+    double valor;
+
+    // Obter informações do usuário
+    printf("Informe o numero da conta de origem: ");
+    scanf("%s", numContaOrigem);
+
+    printf("Informe o numero da conta de destino: ");
+    scanf("%s", numContaDestino);
+
+    printf("Informe o valor a ser transferido: ");
+    scanf("%lf", &valor);
+
+    // Chamar a função de transferência
+    transferirSaldo(numContaOrigem, numContaDestino, valor);
+
+    return 0;
+  
 }
