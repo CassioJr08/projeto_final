362e188e2b38a244e52b33094e3a44dd563a6c80
Joelma Giovanna <giovannabarbosah38@gmail.com>
2023-12-06 08:58:29 -0300

    primeira implementação da funcionalidade

--- funcionalidades/1_CriarNovaConta.c
+++ funcionalidades/1_CriarNovaConta.c
@@ -1,3 +1,23 @@
-void criarConta() {
+#include <stdio.h>
+#include <string.h>
 
+typedef struct {
+    char numeroConta[9];
+    char titularConta[100];
+    double saldoConta;
+} cabeçalhoConta;
+
+void criarConta() {
+    cabeçalhoConta novaConta;
+
+    printf("Digite o número da conta: ");
+    scanf("%s", novaConta.numeroConta);
+
+    printf("Digite o nome do titular: ");
+    scanf("%s", novaConta.titularConta);
+
+    novaConta.saldoConta = 0.0;
+
+    printf("Saldo inicial: %.2lf\n", novaConta.saldoConta);
 }
+
